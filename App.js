c2b426f8670faf58da6bb6bac65210f3d92ad546
Karim <karim.ayoub.job@gmail.com>
2025-07-10 10:51:50 +0200

    Add pages

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function App() {
+
+import Login from "./src/surfaces/Login";
+import { Feed } from "./src/surfaces/Feed";
+import { Conversations } from "./src/surfaces/Conversations";
+import { AddPost } from "./src/surfaces/AddPost";
+import { Favorites } from "./src/surfaces/Favorites";
+import { Profile } from "./src/surfaces/Profile";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Feed") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Conversations") {
+            iconName = focused ? "chatbox" : "chatbox-outline";
+          } else if (route.name === "AddPost") {
+            iconName = focused ? "add-circle" : "add-circle-outline";
+          } else if (route.name === "Favorites") {
+            iconName = focused ? "heart" : "heart-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person-circle" : "person-circle-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#25AD80",
+        tabBarInactiveTintColor: "#000000",
+      })}
+    >
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Conversations" component={Conversations} />
+      <Tab.Screen name="AddPost" component={AddPost} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [userLoggedIn, setIsUserLoggedIn] = useState(true);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {!userLoggedIn ? (
+          <Stack.Screen name="Login" component={Login} />
+        ) : (
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/surfaces/AddPost.js
@@ -0,0 +1,11 @@
+// ./src/surfaces/AddPost.js
+import React from "react";
+import { View, Text } from "react-native";
+
+export const AddPost = () => {
+  return (
+    <View>
+      <Text>this will be the AddPost screen</Text>
+    </View>
+  );
+};
